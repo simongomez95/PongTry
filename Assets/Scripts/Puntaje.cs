97a28f74a465de07c8069ab6494bae14f0809786
unknown <simongomez95@gmail.com>
2016-03-30 17:19:21 -0500

    Agregado scoreboard

--- Assets/Scripts/Puntaje.cs
+++ Assets/Scripts/Puntaje.cs
@@ -7,14 +7,20 @@ public class Puntaje : MonoBehaviour {
     public int puntaje2;
     public GUIStyle estilo;
 
-    void OnGUI()
+    void Update()
+    {
+        string texto = puntaje1 + " - " + puntaje2;
+        this.GetComponent<TextMesh>().text = texto;
+    }
+
+    /*void OnGUI()
     {
         float xMid = Screen.width / 2f;
         float yMid = 300f;
 
         float width = 300f;
         float height = 2000f;
-        string texto = puntaje1 + "   /   " + puntaje2;
+        
         GUI.Label(new Rect(xMid - width / 2f, yMid, width, height), texto, estilo);
-    }
+    }*/
 }
