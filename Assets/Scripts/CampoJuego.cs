5e0798ff3caaa5f86541aca2258ef37ef1a5f281
simon gomez <nekothecat@MacBook-de-simon.local>
2016-04-04 07:54:52 -0500

    agregar prefabs oops

---
+++ Assets/Scripts/CampoJuego.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CampoJuego : MonoBehaviour {
+    
+    public bool invertido = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(invertido) {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 180f);
+            StartCoroutine(DesInvertir());
+        }
+        else {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0f);
+        }
+	
+	}
+    
+    IEnumerator DesInvertir() {
+        
+        yield return new WaitForSeconds(5);
+        
+        invertido = false;
+        
+    }
+}
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	
+	}
+    
+    void OnCollisionEnter(Collision coll) {
+        
+        if(coll.gameObject.tag == "Bola") {
+            CampoJuego cj = GameObject.FindGameObjectWithTag("Field").GetComponent<CampoJuego>();
+            cj.invertido = true;
+            Destroy(gameObject);
+        }
+        
+    }
+}
