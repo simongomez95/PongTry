a2128a6036b13a05c8afbb01a421bad1d1272bca
lope391 <lope1996@gmail.com>
2016-05-12 10:16:34 -0500

    Animacion Rotacion Paleta

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -7,7 +7,12 @@ public class Jugador : MonoBehaviour {
 
     public float vel = 60;
     public float percentDrop = 5f;
-    
+    public float percentBreak = 50f;
+    public int dirR = -1;
+    public int pasoR = 1;
+    Vector3 posFinal, posActual;
+
+
     public GameObject powerup;
     
     public bool agrandado = false;
@@ -28,26 +33,60 @@ public class Jugador : MonoBehaviour {
         }
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name == "Bola")
-        {
-            collision.rigidbody.AddForce(collision.rigidbody.velocity.normalized * 1.1f, ForceMode.Impulse);
-            //Decidir si se crea o no powerup
+    void OnCollisionEnter(Collision collision) {
+
+        if (collision.gameObject.name == "Bola") {
+
             float rand = Random.Range(0f, 100f);
+
+            //Decidir si cambia al breakout
+            if (rand < percentBreak) {
+                collision.rigidbody.velocity = Vector3.zero;
+
+                //calcula posición actual y final de rotacion de la paleta    
+                posActual = transform.eulerAngles;
+                posFinal.x = (posActual.x + (90 * dirR));
+
+                //comienza a rotar
+                StartCoroutine(CambiarEscena(collision.gameObject));          
+            }
+
+            collision.rigidbody.AddForce(collision.rigidbody.velocity.normalized * 1.1f, ForceMode.Impulse);
+            
+            //Decidir si se crea o no powerup
+            rand = Random.Range(0f, 100f);
             if (rand < percentDrop) {
                 InstanciarPowerup();
             }
         }
     }
+
+    
     
     void InstanciarPowerup() {
         Instantiate(powerup, new Vector3(transform.position.x, transform.position.y,
          transform.position.z + Random.Range(5f, 32f)), Quaternion.identity);
     }
-    
-    
-    
+
+    IEnumerator CambiarEscena(GameObject bola) {
+
+        //suma paso a paso la rotacion cada frame
+        posActual.x += (pasoR * dirR);
+        transform.eulerAngles = posActual;
+
+        //girar la bola alrededor del eje de la paleta
+        Vector3 posBola = bola.transform.position;
+        Vector3 posPal = transform.position;
+
+        yield return new WaitForSeconds(0);
+
+        //checkea si ya termino de rotar tanto para en el sentido de las manecillas del reloj como en contra
+        if ((int)posActual.x > (int)posFinal.x || (int)posActual.x > (int)posFinal.x) {
+            StartCoroutine(CambiarEscena(bola));
+        }
+
+    }
+
     IEnumerator DesAgrandar() {
         
         yield return new WaitForSeconds(5);
