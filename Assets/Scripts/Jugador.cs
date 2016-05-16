40f11f5843654467a48f6bb30244f2b460358b17
lope391 <lope1996@gmail.com>
2016-05-15 20:00:37 -0500

    Rotacion V2 bola paleta, problemas con bola

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -27,16 +27,23 @@ public class Jugador : MonoBehaviour {
 		if(collision.gameObject.name == "Bola"){
 			float rand = Random.Range(0f, 100f);
 			//Decidir si cambia al breakout
-			/*
+			
             if(rand<percentBreak){
+                //detiene la pelota
                 collision.rigidbody.velocity = Vector3.zero;
+                collision.rigidbody.angularVelocity = Vector3.zero;
+
                 //calcula posición actual y final de rotacion de la paleta    
                 posActual = transform.eulerAngles;
                 posFinal.x = (posActual.x + (90 * dirR));
+                
                 //comienza a rotar
-                StartCoroutine(CambiarEscena(collision.gameObject));          
+                StartCoroutine(CambiarEscena());
+
+                collision.rigidbody.velocity = Vector3.zero;
+                collision.rigidbody.angularVelocity = Vector3.zero;
             }
-            */
+            
 			//Calculo nueva velocidad de la bola basada en las velocidades de la bola y la paleta al chocar
 			var velo = collision.rigidbody.velocity;
 			velo.x = (velo.x/2.0f)+(this.GetComponent<Rigidbody>().velocity.x+3.0f);
@@ -58,17 +65,17 @@ public class Jugador : MonoBehaviour {
 		transform.position.z + Random.Range(5f, 32f)), Quaternion.identity);
 	}
 
-	IEnumerator CambiarEscena(GameObject bola){
+	IEnumerator CambiarEscena(){
 		//suma paso a paso la rotacion cada frame
 		posActual.x += (pasoR * dirR);
-		transform.eulerAngles = posActual;
-		//girar la bola alrededor del eje de la paleta
-		Vector3 posBola = bola.transform.position;
-		Vector3 posPal = transform.position;
+		transform.parent.gameObject.transform.eulerAngles = posActual;
+        //girar la bola alrededor del eje de la paleta
+        //Vector3 posBola = bola.transform.position;
+        Vector3 posPal = transform.position;
 		yield return new WaitForSeconds(0);
 		//checkea si ya termino de rotar tanto para en el sentido de las manecillas del reloj como en contra
 		if ((int)posActual.x > (int)posFinal.x || (int)posActual.x > (int)posFinal.x) {
-			StartCoroutine(CambiarEscena(bola));
+			StartCoroutine(CambiarEscena());
 		}
 
 	}
